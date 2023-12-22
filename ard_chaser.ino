fdfb3f429c1832e90aa2edfb4bb69c1314e2ea20
IlhamAriPrastyo <148432155+IlhamAriPrastyo@users.noreply.github.com>
2023-12-22 11:13:47 +0700

    Update ard_chaser.ino

--- ard_chaser.ino
+++ ard_chaser.ino
@@ -1,97 +1,44 @@
-// IF you are unable to upload the file to Arduino then uncomment the "Adafruit_SSD1306.h"
-// and "SimpleDHT.h" 
-//#include <Adafruit_SSD1306.h>
-//#include <SimpleDHT.h>
-#include <LiquidCrystal.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
-#define led 6
-#define led1 7
-#define led2 8
-#define led3 9
-#define led4 10
-int data, flag = 2;
+LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+// Define LED pins
+const int ledPin3 = 3;  // Pin for LED when ID is 2
+const int ledPin4 = 4;  // Pin for LED when ID is 4 or 1
 
-void setup()
-{
-  pinMode(led, OUTPUT);
-  pinMode(led1, OUTPUT);
-  pinMode(led2, OUTPUT);
-  pinMode(led3, OUTPUT);
-  pinMode(led4, OUTPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
+void setup() {
   Serial.begin(9600);
-  digitalWrite(led, LOW);
-  digitalWrite(led1, LOW);
-  digitalWrite(LED_BUILTIN, LOW);
-  lcd.begin(16, 2);                       
-  lcd.setCursor (0,0);                   
-  lcd.print("                ");
-  lcd.setCursor (0,1);
-  lcd.print("                ");
+  lcd.begin();
+  lcd.backlight();  // Enable LCD backlight
+  
+  // Set LED pins as OUTPUT
+  pinMode(ledPin3, OUTPUT);
+  pinMode(ledPin4, OUTPUT);
 }
 
-void loop()
-{
-  while( Serial.available() )
-  {
-    data = Serial.read();
+void loop() {
+  if (Serial.available() > 0) {
+    char command = Serial.read();
+    lcd.clear();
+    lcd.setCursor(0, 0);
 
-    if (data == '1')
-    {
-      flag = 1;
-    }
-    else if(data == '0')
-    {
-      flag = 0;
+    if (command == '1') {
+      lcd.print("Selamat");
+      lcd.setCursor(0, 1);
+      lcd.print("Datang");
+      digitalWrite(ledPin4, LOW);  // Turn off LED on pin 4
+      digitalWrite(ledPin3, HIGH);  // Turn on LED on pin 3
+    } else if (command == '2') {
+      lcd.print("Ada");
+      lcd.setCursor(0, 1);
+      lcd.print("Tamu");
+      digitalWrite(ledPin3, LOW);  // Turn off LED on pin 3
+      digitalWrite(ledPin4, HIGH);  // Turn on LED on pin 4
+    } else if (command == '0') {
+      lcd.print("penyusup");
+      digitalWrite(ledPin3, LOW);  // Turn off LED on pin 3
+      digitalWrite(ledPin4, LOW);  // Turn off LED on pin 4
     }
   }
-  if(flag == 1)
-    {
-      lcd.setCursor (0,0);
-      lcd.print(" Welcome Boss!!  ");
-      digitalWrite(led, HIGH);
-      digitalWrite(led1, LOW);
-      digitalWrite(led2, LOW);
-      digitalWrite(led3, LOW);
-      digitalWrite(led4, LOW);
-      delay(200);
-      digitalWrite(led, LOW);
-      digitalWrite(led1, HIGH);
-      digitalWrite(led2, LOW);
-      digitalWrite(led3, LOW);
-      digitalWrite(led4, LOW);
-      delay(200);
-      digitalWrite(led, LOW);
-      digitalWrite(led1, LOW);
-      digitalWrite(led2, HIGH);
-      digitalWrite(led3, LOW);
-      digitalWrite(led4, LOW);
-      delay(200);
-      digitalWrite(led, LOW);
-      digitalWrite(led1, LOW);
-      digitalWrite(led2, LOW);
-      digitalWrite(led3, HIGH);
-      digitalWrite(led4, LOW);
-      delay(200);
-      digitalWrite(led, LOW);
-      digitalWrite(led1, LOW);
-      digitalWrite(led2, LOW);
-      digitalWrite(led3, LOW);
-      digitalWrite(led4, HIGH);
-      delay(200);
-      digitalWrite(LED_BUILTIN, HIGH);
-    }
-     else if (flag == 0)
-    {
-      lcd.setCursor (0,0);
-      lcd.print("Face Unidentified  ");
-      digitalWrite(led, LOW);
-      digitalWrite(led1, LOW);
-      digitalWrite(led2, LOW);
-      digitalWrite(led3, LOW);
-      digitalWrite(led4, LOW);
-      digitalWrite(LED_BUILTIN, LOW);
-    }
 }
